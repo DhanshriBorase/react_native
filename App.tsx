634cd1a7e3d33e09336e4f17d4022ac77522d3d8
DhanshriBorase <dhanashri1371@gmail.com>
2024-09-20 21:35:57 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import AppNavigator from './src/navigation/AppNavigator';
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <AppNavigator />
-//     </NavigationContainer>
-//   );
-// }
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
